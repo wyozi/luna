a681aa92b2420690bd48c54517b204e6c17ee0c8
wyozi <joonasmertanen@gmail.com>
2016-06-12 00:41:23 +0300

    Improved ast print

--- luna.lua
+++ luna.lua
@@ -21,23 +21,45 @@ elseif args[1] == "ast" then
 	local p = require("src/parser").new(l)
 	local block = p:block()
 	
-	local function printt(t, i)
-		for k,v in pairs(t) do
-			if not ({line=true, col=true})[k] then
-				io.write((" "):rep(i or 0))
-				io.write(tostring(k))
-				io.write(" = ")
+	local function printnode(t, i)
+		local indent = ("  "):rep(i or 0)
+		local indentn = ("  "):rep((i or 0) + 1)
 
-				if type(v) == "table" then
-					print()
-					printt(v, (i or 0) + 1)
-				else
-					print(tostring(v))
+		local function printkv(k, v)
+			io.write(indent)
+			io.write(tostring(k))
+			io.write(" = ")
+
+			if type(v) == "table" then
+				print()
+				printnode(v, (i or 0) + 1)
+			else
+				print(tostring(v))
+			end
+		end
+
+		if t.type then
+			local s = string.format("[%s at line %d; col %d]", t.type, t.line or -1, t.col or -1)
+			if t.type == "identifier" or t.type == "literal" then
+				print(indent .. s .. " = " .. t.text)
+			else
+				print(indent .. s .. " {")
+				for k,v in ipairs(t) do
+					if type(v) == "table" then
+						printnode(v, (i or 0) + 1)
+					else
+						print(indentn .. tostring(v))
+					end
 				end
+				print(indent .. "}")
+			end
+		else
+			for k,v in pairs(t) do
+				printkv(k, v)
 			end
 		end
 	end
-	printt(block)
+	printnode(block)
 elseif args[1] == "run" then
 	local l = require("src/lexer").new(loadInput())
 	local p = require("src/parser").new(l)
